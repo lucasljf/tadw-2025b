5e7cc91234c9cf979255756616244147037651c6
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-09-25 09:47:56 -0300

    Exemplo: carrinho de compras

---
+++ codigo/exemplo-carrinho/carrinho.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+require_once "../conexao.php";
+require_once "../funcoes.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- ver carrinho atual -->
+    <?php
+    if (empty($_SESSION['carrinho'])) {
+        echo "carrinho vazio";
+    } else {
+        $total = 0;
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<td>Tipo</td>";
+        echo "<td>Nome</td>";
+        echo "<td>Preço</td>";
+        echo "<td>Quantidade</td>";
+        echo "<td>Total unitário</td>";
+        echo "<td>Remover</td>";
+        echo "</tr>";
+        foreach ($_SESSION['carrinho'] as $id => $quantidade) {
+            $produto = pesquisarProdutoId($conexao, $id);
+
+            echo "<tr>";
+            echo "<td>" . $produto['tipo'] . "</td>";
+            echo "<td>" . $produto['nome'] . "</td>";
+            echo "<td> R$ " . $produto['preco_venda'] . "</td>";
+            echo "<td>$quantidade</td>";
+            
+            $total_unitario = $produto['preco_venda'] * $quantidade;
+            $total += $total_unitario;
+            
+            echo "<td> R$ $total_unitario</td>";
+            echo "<td><a href='remover.php?id=$id'>[remover]</a></td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        echo "<h3>Total da compra: R$ $total </h3>";
+    }
+    ?>
+
+    <p>
+        <a href="index.php">Adicionar produtos</a>
+    </p>
+</body>
+
+</html>
---
+++ codigo/exemplo-carrinho/index.php
@@ -0,0 +1,42 @@
+<?php
+require_once "../funcoes.php";
+require_once "../conexao.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- link para destrui o carrinho e simular um novo início -->
+    <a href="destruir_carrinho.php">destruir carrinho</a>
+
+    <!-- ver produtos que podem ser adicionados -->
+    <form action="adicionar.php" method="POST">
+        <h2>Listagem de Produtos</h2>
+
+        <ul>
+            <?php
+                $produtos = listarProdutos($conexao);
+
+                foreach ($produtos as $produto):
+            ?>
+            <li>
+                <input type="checkbox" name="idproduto[]" value="<?php echo $produto['idproduto'] ?>"> R$ <span><?php echo $produto['preco_venda']; ?></span> -- <?php echo $produto['nome']; ?>
+
+                <input type="number" name="quantidade[<?php echo $produto['idproduto'];?>]" value="1" min="1">
+            </li>
+            <?php endforeach; ?>
+        </ul>
+
+        <input type="submit" value="Adicionar selecionados ao carrinho">
+    </form>
+
+    <a href="carrinho.php">Ver carrinho</a>
+</body>
+
+</html>

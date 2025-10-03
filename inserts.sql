4bb431b3d30887565052166bc51b2158b6e22427
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-10-03 18:25:17 -0300

    Update inserts.sql
    Alteração nos tipos dos itens.

--- inserts.sql
+++ inserts.sql
@@ -2,10 +2,10 @@ INSERT INTO tb_cliente (nome, cpf, endereco) VALUES ("Fulano", "111.222.333-44",
 INSERT INTO tb_cliente (nome, cpf, endereco) VALUES ("Ciclano", "222.222.333-44", "Rua 20");
 
 INSERT INTO tb_produto (nome, tipo, preco_compra, preco_venda, margem_lucro, quantidade)
-VALUES ("Coca-cola", "Alimento", 5.50, 11.00, 0, 200);
+VALUES ("Coca-cola", "Bebida", 5.50, 11.00, 0, 200);
 
 INSERT INTO tb_produto (nome, tipo, preco_compra, preco_venda, margem_lucro, quantidade)
-VALUES ("Carvão", "Alimento", 9.50, 13.00, 0, 100);
+VALUES ("Carvão", "Utilidade", 9.50, 13.00, 0, 100);
 
 INSERT INTO tb_produto (nome, tipo, preco_compra, preco_venda, margem_lucro, quantidade)
 VALUES ("Picanha", "Alimento", 41.10, 65.00, 0, 138);
